a537f0e6d1bbde1d6a4fb74e0b94350bde5e3256
ramironavas <ramiroanibal.0611@gmail.com>
2025-11-17 13:19:46 +0100

    create string examples v2

--- js/string.js
+++ js/string.js
@@ -2,7 +2,7 @@
 
 // 1. EJEMPLO SLICE //
   
-  const texto1 = "Hola Mundo, bienvenido a JavaScript!";
+  const texto1 = "Hola equipo, este es un ejemplo.";
     const pOriginal1 = document.getElementById("texto1original");
     const pCortado1 = document.getElementById("texto1cortado");
     const btn1 = document.getElementById("cortarPalabra");
@@ -17,9 +17,9 @@
     });
 
 
-    //2. EJEMPLO CHAR-AT //
+//2. EJEMPLO CHAR-AT //
 
-    const texto2 = "Hola Mundo";
+    const texto2 = "Bootcamp Peñascal F5 2025";
     const pOriginal2 = document.getElementById("texto2original");
     const input = document.getElementById("indice");
     const btn2 = document.getElementById("mostrarChar");
@@ -32,3 +32,4 @@
       const c = texto2.charAt(i);
       pChar.textContent = `Carácter en el índice ${i}: "${c}"`;
 });
+
