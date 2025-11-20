8d19d2aea1f016e0e3fca0298cfd25bccd0beb2e
ylcruzdev <ylcruz.dev@gmail.com>
2025-11-20 09:52:51 +0100

    comming son example json stringify

---
+++ js/jsonStringify.js
@@ -0,0 +1,68 @@
+//pasando un objeto solamente
+let objectJs = {
+  foundation: "Mozilla",
+  model: "box",
+  week: 45,
+  transport: "car",
+  month: 7,
+};
+let jsonString = JSON.stringify(objectJs);
+
+console.log(jsonString);
+
+
+//usando una funcion para el parametro replacer
+
+function replacer(key, value) {
+  // Filtrando propiedades
+  return typeof value === "string" ? undefined : value
+
+}
+
+let objectJs2 = {
+  name: "jon",
+  skill: "javaScript",
+  task: 30,
+  age: "21",
+  projects: 3,
+  commits: 200
+};
+let jsonString2 = JSON.stringify(objectJs2, replacer);
+console.log(jsonString2);
+// salida: '{"task":30, "projects":3, "commits": 200}'
+
+
+//Si el reemplazo es un array, los valores indican los nombres de las propiedades del objeto que se va a incluir en la cadena JSON resultado.
+let jsonString3 = JSON.stringify(objectJs2, ["name", "skill", "age"]);
+console.log(jsonString3);
+// salida: '{"name":"jon","skill":"javascript","age":"21"}', sólo mantiene las propiedades de "name", "skill" y "age"
+
+//usando argumento space
+let jsonString4 = JSON.stringify(objectJs2, null, " ");
+console.log(jsonString4);
+/* salida:
+{
+ "name": "jon",
+ "skill": "javaScript",
+ "task": 30,
+ "age": "21",
+ "projects": 3,
+ "commits": 200
+}
+*/
+
+
+//Usar el carácter tabulador simula la apariencia de impresión:
+let jsonString5 = JSON.stringify(objectJs2, null, "\t");
+console.log(jsonString5);
+
+/* devuelve el string:
+{
+    "name": "jon",
+    "skill": "javaScript",
+    "task": 30,
+    "age": "21",
+    "projects": 3,
+    "commits": 200
+}
+*/
